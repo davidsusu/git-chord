92b3d10c8ec06379e99ead761e133af9f5fe21cf
Dávid Horváth <horvath@webarticum.hu>
2024-09-17 22:22:42 +0200

    Add a netinstaller script

---
+++ install/netinstall-user.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+GIT_URL='https://github.com/davidsusu/git-chord'
+
+startDir="$( realpath "$( pwd )" )"
+installDir="${HOME}/git-chord"
+mkdir -p "$installDir"
+
+cd "$installDir"
+
+git clone "$GIT_URL" .
+
+# TODO: decide which installer to use
+./install/install-user-bashrc-path.sh
+
+cd "$startDir"
